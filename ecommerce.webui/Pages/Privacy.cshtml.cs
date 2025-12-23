c06b5ae2f0b731d2ea48bd655707e538bc494ea2
Srkn <stoy@windowslive.com>
2025-12-23 22:18:35 +0300

    Proje İlk Hali

---
+++ ecommerce.webui/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ecommerce.webui.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
