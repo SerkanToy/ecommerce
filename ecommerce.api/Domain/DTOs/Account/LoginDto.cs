c60e2ca047cf011c59dac3b55b9ceb316aaa5481
Srkn <stoy@windowslive.com>
2026-02-05 00:23:24 +0300

    Create Token Yapılandırıldı

---
+++ ecommerce.api/Domain/DTOs/Account/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ecommerce.api.Domain.DTOs.Account
+{
+    public class LoginDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public bool RememberMe { get; set; } = true;
+        public List<string> Errors { get; set; } = new List<string>();
+    }
+}
